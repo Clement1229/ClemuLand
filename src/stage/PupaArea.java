0f9460b9b70352a796effe8f50c3bdbc102c68f9
Clement Lin <x4383lin@gmail.com>
2017-04-13 20:53:11 -0500

    generating monsters for PupaArea

--- src/stage/PupaArea.java
+++ src/stage/PupaArea.java
@@ -1,16 +1,15 @@
 package stage;
 
 import champion.Champion;
-import monsters.BuggyFly;
-import monsters.Monsters;
-import monsters.Poly;
-import monsters.Slime;
+import monsters.*;
 
 public class PupaArea extends Stage{
-	int pupa;
-	int slime;
-	int poly;
-	int BuggyFly;
+	
+	int slime;    // 6%
+	int poly;     // 6%
+	int pupa;     // 50%
+	int fabre;    // 30%
+	int BuggyFly; // 8%
 	int x;
 	int y;
 	
@@ -20,9 +19,11 @@ public class PupaArea extends Stage{
 	}
 	
     public void monsterGenerator(){
-		slime = 89; // 0~89
-		poly = 90;  // 90~99
-		BuggyFly = 100; //10011
+		slime = 5;  // 0-5
+		poly = 11;  // 6-11
+		pupa = 51;  // 12-62
+		fabre = 81; // 63-93
+		BuggyFly = 100; //94-100
 	}
 //****************************adventure*******************************
     public void adventure(Champion c){ // mons 86%  item 14%
@@ -37,24 +38,40 @@ public class PupaArea extends Stage{
     
     public void adventure_mosnters(Champion c){
     	int value = ram.nextInt(101);
-        if (value < 90){ // slime
+        if (value <= 5){ // slime
         	Monsters slime = new Slime();
-        	System.out.println("log: random number: " + value);
+        	//System.out.println("log: random number: " + value);
         	System.out.println("You encounter with a Slime");
         	System.out.println("1. attack 2. item 3.escape");
         	int battle_n= scan.nextInt();
         	battleCondition(battle_n, c, slime);
         }
-        if  (value >= 90 && value < 100){ // poly
-        	System.out.println("random number: " + value);
+        if  (value >= 6 && value <= 11){ // poly
+        	//System.out.println("random number: " + value);
         	System.out.println("You encounter with a Poly");
         	System.out.println("1. attack 2. item 3.escape");
         	Monsters poly = new Poly();
         	int battle_n= scan.nextInt();
         	battleCondition(battle_n, c, poly);
         }
-        if (value == 100){ //BuggyFly
-        	System.out.println("random number: " + value);
+        if  (value >= 12 && value <= 62){ // pupa
+        	//System.out.println("random number: " + value);
+        	System.out.println("You encounter with a Pupa");
+        	System.out.println("1. attack 2. item 3.escape");
+        	Monsters pupa= new Pupa();
+        	int battle_n= scan.nextInt();
+        	battleCondition(battle_n, c, pupa);
+        }
+        if  (value >= 63 && value <= 93){ // fabre
+        	//System.out.println("random number: " + value);
+        	System.out.println("You encounter with a Fabre");
+        	System.out.println("1. attack 2. item 3.escape");
+        	Monsters fabre = new Fabre();
+        	int battle_n= scan.nextInt();
+        	battleCondition(battle_n, c, fabre);
+        }
+        if (value >= 94 && value <= 100){ //BuggyFly
+        	//System.out.println("random number: " + value);
         	System.out.println("You encounter with....");
         	System.out.println( " a BuggyFly !!!!");
         	System.out.println("1. attack 2. item 3.escape");
