b3546bb35cb8c091481e47941b191c23f7fb4d68
Clement Lin <x4383lin@gmail.com>
2017-09-15 16:29:37 -0400

    Equipment class created

---
+++ src/item/Equipment.java
@@ -0,0 +1,22 @@
+package item;
+
+public enum Equipment {
+	
+	headgear,armor,weaponL,weaponR,garment, AccessoryL, AccessoryR;
+	
+	String name;
+	int buyPrice;
+	int sellPrice;
+	int atk;
+	int ReqLevel; //required level
+	
+	public void wear(){
+		
+	}
+	public void takeOff(){
+		
+	}
+
+}
+
+
